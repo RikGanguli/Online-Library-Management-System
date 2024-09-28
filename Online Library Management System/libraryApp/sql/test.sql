c2663c43b8779bea9a851555ef93c2bf1a9bd957
Rik Ganguli Biswas <rikganguli52@gmail.com>
2024-09-28 13:36:51 -0500

    Delete Online Library Management System directory

--- Online Library Management System/libraryApp/sql/test.sql
+++
@@ -1,7 +0,0 @@
-SELECT * FROM loginaccess;
-SELECT * FROM memlogin;
-SELECT * FROM liblogin;
-select * from members;
-select * from membership;
-select * from librarians;
-select * from members inner join membership using (mid) where mid = 1;
